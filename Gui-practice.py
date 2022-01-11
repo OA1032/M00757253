bf1cd1b2a800b3c4c768b808d949fe888e05813f
OA1032 <OA1032@live.mdx.ac.uk>
2022-01-11 14:53:38 +0000

    Create Gui-practice.py
    Some more practice with conversion of input data types.

---
+++ Gui-practice.py
@@ -0,0 +1,8 @@
+value = input("Welcome to the menu. Otions are listed blelow!: ")
+print ("The value you input is:", value)
+print (f'it is of the type, {type(value)}.')
+
+value = int(value)
+
+print ("The value you input is:", value)
+print (f'it is of the type, {type(value)}.')
