7b56bcb3bd20788ea47a9bc898b145e41b8b70b1
OA1032 <OA1032@live.mdx.ac.uk>
2022-01-31 15:16:11 +0000

    Small flag selection additions.

--- Develop/Flags.py
+++ Develop/Flags.py
@@ -9,6 +9,7 @@
 colours of the flag and led position in each row
 MAybe make it a function so it doesnt have to be repeated constaantly
 most likely will have to do so for each individual flag..... RIP'''
+from pyparsing import alphas
 import opc
 import time
 
@@ -118,13 +119,20 @@ for i in range(10,51,1):
 for i in range(0,360):
     #Led printing stating point
     #if led = range()
-        colour[i] =(0,255,255)
-        client.put_pixels(colour)
+    colour[i] =(0,255,255)
+    client.put_pixels(colour)
     #print("i =",i)
-    if i == 60 or i ==120 or i == 180 or i == 240 or i == 300:
-        time.sleep(0.1)
+    if i == 59 or i ==119 or i == 179 or i == 239 or i == 299:
+        time.sleep(1)
         print('sleep')
 '''
+'''q = 0
+while q > 1:
+    if colour[] == 360
+        q = q + 1'''
+
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Flags_Vertical_Print_Sideways~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
 for i in range(10,51,1):
@@ -275,8 +283,41 @@ for i in range(3):
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~3_level_Slotting_flag_building_Horizontal~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~3_level_Slotting_flag_building_Vertical~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Top_and_bottom_brought_together~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Right_and_left_brought_together~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''for i in range(10,50):
+    print("i",i)
+    colour[i] =(255,255,255)
+    client.put_pixels(colour)
+    colour[i+60] =(255,255,255)
+    client.put_pixels(colour)
+    time.sleep(0.1)
+    x = 60 - i
+    print("x",x)
+    colour[x] =(255,255,255)
+    client.put_pixels(colour)
+    time.sleep(0.1)'''
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~3_part_custom_flag~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #Vertical or Horizontal,Stripe Number(max of three),Background,Forground,Emblem shape(Square,Circle,Triangle,Diamond),Emblem position(Left,Centre,Right)
 # Choose colours for each relevant option from a dictionary of 10 colours or so. build each element from a random transition.
-#
+#Save inputs as a list, store in SQL and repeat when requested.Do same with login details
+
+'''
+
+
+
+def vertical():
+
+def horizontal():
+
+def no_stripes(even_colour(2,4,6),odd_colour(1,3,5)):
+
+def background():
+
+def midground():
+
+def foreground():
+
+def emblem(position):
+    #bird,square,diamon,circle,triangle
+'''
