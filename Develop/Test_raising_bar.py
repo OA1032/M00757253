2d56988c4fade8b875c0588b4ac446ecfbef84bb
OA1032 <OA1032@live.mdx.ac.uk>
2022-02-08 23:08:54 +0000

    More bar animation developments ans improvements

--- Develop/Test_raising_bar.py
+++ Develop/Test_raising_bar.py
@@ -21,7 +21,7 @@ for i in range (300,360):
             colour[x] =(0,255,0)
             client.put_pixels(colour)
             time.sleep(0.06)
-        time.sleep(0.1)
+       # time.sleep(0.1)
 
 for i in range(0,30):
     print("i",i)
@@ -39,4 +39,47 @@ for i in range(359,330,-1):
     client.put_pixels(colour)
     time.sleep(0.05)
     
-        
+for i in range(180,210):
+    print("i",i)
+    colour[i] = (0,0,0)
+    colour[i+60] = (0,0,0)
+    colour[i+120] = (0,0,0)
+    client.put_pixels(colour)
+    time.sleep(0.05)
+
+for i in range(60,30,-1):
+    print("i",i)
+    colour[i] = (0,0,0)
+    colour[i+60] = (0,0,0)
+    colour[i+120] = (0,0,0)
+    client.put_pixels(colour)
+    time.sleep(0.05)
+
+for i in range (143,158):
+    print(i)
+    colour[i] =(0,255,0)
+    client.put_pixels(colour)
+    time.sleep(0.05)
+
+for i in range(360):
+    if i % 2 == 0:
+        print ("i",i)
+        colour[i] =(0,255,0)
+        colour[i+1] =(0,255,0)
+        client.put_pixels(colour)
+        time.sleep(0.05)
+    
+        if i % 4 == 0:
+            print ("i",i)
+            colour[i] =(0,0,0)
+            colour[i+1] =(0,0,0)
+            client.put_pixels(colour)
+            time.sleep(0.05)
+
+for i in range(360):
+    print("i",i)
+    colour[i] =(0,0,0)
+    client.put_pixels(colour)
+    if  i == 59 or i == 119 or i == 179 or i == 239 or i == 299:
+        print("i",i)
+        time.sleep(0.5)
